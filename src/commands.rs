a5a2400587253e12b97a9c4f214e7fdb20d8ccbe
Daniel Alder <daald@users.noreply.github.com>
2025-09-29 12:48:34 +0200

    Cleanup

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,7 @@
 use crate::CommBulk;
 use hex_literal::hex;
 use log::debug;
-use serde::{Deserialize, Serialize}; //use: hex!
+use serde::{Deserialize, Serialize};
 
 pub fn cmd_nmea_switch(comm: &mut CommBulk, flag: bool) {
     debug!("Send cmd_nmea_switch");
@@ -24,7 +24,7 @@ pub fn cmd_model(comm: &mut CommBulk) -> Model {
     debug!("Send cmd_model");
     let command: Vec<u8> = hex!["9305040003019f"].to_vec();
 
-    let answer = comm.simple_cmd_return(command); //[0x93,0x00,0x03,0xc2,0x20,0x15,0x73].to_vec());
+    let answer = comm.simple_cmd_return(command);
 
     if answer.len() != 3 || answer[0] != 0xc2 || answer[1] != 0x20 {
         panic!("Unexpected answer: {answer:02x?}");
@@ -84,8 +84,7 @@ pub fn cmd_identification(
 
     let name2 = format!("{:02X}{:02X}", answer[11], answer[10]);
     let version = format!("{version1}.{version2}.{version3y:02}{version3m:02}{version3d:02}");
-    // this is far away from perfect!
-    let model = u16::from_be_bytes(answer[6..8].try_into().unwrap()); //+ "-" + hex::encode(answer[10..16]); // todo: leading zeroes and reverse order of bytes
+    let model = u16::from_be_bytes(answer[6..8].try_into().unwrap());
     let devid2 = u64::from_le_bytes(answer[8..16].try_into().unwrap()) >> 16 & 0xffffffffffff;
     let serialnumber = 10000000000u64 * model as u64 + serial as u64;
     let deviceid = format!("{model:04}-{devid2:012X}");
@@ -144,7 +143,6 @@ pub fn cmd_set_time(comm: &mut CommBulk, time_us: u64) {
     debug!("Send cmd_set_time");
     let mut command: Vec<u8> = hex!["9309"].to_vec();
 
-    //let time_us = 1753997870971000_u64;
     let time_s = time_us / 1_000_000;
 
     command.extend(&time_us.to_le_bytes()[0..8]);
@@ -155,20 +153,12 @@ pub fn cmd_set_time(comm: &mut CommBulk, time_us: u64) {
 
 pub fn cmd_read(comm: &mut CommBulk, pos: u32, size: u16) -> Vec<u8> {
     debug!("Send cmd_read (size: {size:04x}  pos: {pos:06x}");
-    let mut command: Vec<u8> = hex!["930507"].to_vec(); //,0,0,0,0,0,0,0];
-
-    //    command[3] = (size >> 0x08) & 0xff;
-    //    command[4] = (size >> 0x00) & 0xff;
-    //    command[7] = (pos >> 0x10) & 0xff;
-    //    command[8] = (pos >> 0x08) & 0xff;
-    //    command[9] = (pos >> 0x00) & 0xff;
+    let mut command: Vec<u8> = hex!["930507"].to_vec();
 
     command.extend(&size.to_be_bytes());
     command.push(0x04);
     command.push(0x03);
     command.extend(&pos.to_be_bytes()[1..4]);
-    //command[3..5]  = size.to_be_bytes();
-    //command[7..10] = pos.to_be_bytes()[1..4];
 
     let answer = comm.simple_cmd_return(command);
 
